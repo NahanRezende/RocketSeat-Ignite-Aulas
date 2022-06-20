c61f8a3cef768df3bd864b9903c89f84df5ae206
NahanRezende <nahan.rezende@aluno.ufop.edu.br>
2022-06-19 21:51:33 -0300

    feat: Validating if customer already exists

--- IgniteReactClasses/FirstProject/src/index.js
+++ IgniteReactClasses/FirstProject/src/index.js
@@ -3,17 +3,21 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
 const customers = [];
 
-app.post('/account', (req, res) => {
+app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
-  const id = uuidv4();
+  if(customers.some(customer => customer.cpf === cpf)){
+    return response.status(400).json({error: "Customer already exists!"});
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   });
 
